a7c18de558c729f28e7dfa3723518ceb96966fb8
Zeljko Bajsanski <zeljko.bajsanski@enlightit.rs>
2018-04-09 11:00:53 +0200

    Initial

---
+++ src/pages/home/home.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import * as Raphael from 'raphael';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  paper;
+  image;
+  height;
+  width;
+  isRunning = false;
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  ionViewDidLoad() {
+    this.width = $(".scroll-content").width();
+    this.height = $(".scroll-content").height();
+    this.paper = Raphael("content",  this.width, this.height);
+    const text = this.paper.text(70, 20, "16 - 6 = ?");
+    text.attr("font-size", 32);
+    
+  }
+
+  start() {
+    if (this.isRunning) return;
+    this.image = this.paper.image("assets/imgs/parachute.png", this.width / 2 - 32, 0, 64, 64);
+    let i = 0;
+    const tries = 10;
+    const dy = this.height / 10;
+    const handler = setInterval(() => {
+      i++;
+      this.image.translate(0, dy);
+      if (i == tries) {
+        clearInterval(handler);
+        this.isRunning = false;
+      }
+    }, 2000);
+    this.isRunning = true;
+  }
+
+}
